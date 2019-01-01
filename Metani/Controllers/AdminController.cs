ff589ef50413419cdf6cea5065ad4821a917139c
Misbahul Ardani <misbahulard@gmail.com>
2019-01-01 19:27:54 +0700

    Final commit yey!

--- Metani/Controllers/AdminController.cs
+++ Metani/Controllers/AdminController.cs
@@ -13,7 +13,42 @@ namespace Metani.Controllers
         public ActionResult Index()
         {
             if ((bool) Session["Login"])
-                return View();
+            {
+                MetaniContext context = new MetaniContext();
+                MapTani mapTani = new MapTani();
+                List<MapList> hasilTaniList = new List<MapList>();
+                List<HasilTaniJoin> hasilTani = context.GetAllHasilTaniJoin();
+                foreach (var item in hasilTani)
+                {
+                    MapList mapList = new MapList();
+                    mapList.nama = item.jenisTani.NamaJenisTani;
+                    mapList.desc = item.Jumlah.ToString();
+                    mapList.latitude = item.lokasi.Latitude;
+                    mapList.longitude = item.lokasi.Longitude;
+
+                    hasilTaniList.Add(mapList);
+                }
+
+                mapTani.HasilTani = hasilTaniList;
+
+                List<MapList> tanahList = new List<MapList>();
+                List<TanahJoin> tanah = context.GetAllTanahJoin();
+                foreach (var item in tanah)
+                {
+                    MapList mapList = new MapList();
+                    mapList.nama = item.JenisTanah.NamaJenisTanah;
+                    mapList.desc = item.LuasTanah.ToString();
+                    mapList.latitude = item.Lokasi.Latitude;
+                    mapList.longitude = item.Lokasi.Longitude;
+
+                    tanahList.Add(mapList);
+                }
+
+                mapTani.Tanah = tanahList;
+
+                return View(mapTani);
+            }
+
             return RedirectToAction("Login");
         }
 
